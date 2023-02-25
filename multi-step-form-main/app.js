824737920dd8b70f4acbb5346807f606fb17d231
itzadn <adrian.cholma@gmail.com>
2023-02-25 16:54:16 +0100

    feat(sidebar/step numbers): add active class on load and on click step handler to button

--- multi-step-form-main/app.js
+++ multi-step-form-main/app.js
@@ -1,24 +1,54 @@
 import stepsJson from './mocks/steps.json' assert { type: 'json' };
 
-const sidebar = document.querySelector('.main__sidebar--svg');
+const sidebar = document.querySelector('.main__sidebar--container');
+const nextStepButton = document.querySelector('.next-step');
+const sidebarStepNumberIcons = sidebar.getElementsByClassName('sidebar__step__number');
 
 window.onload = () => {
 	stepsJson.data.map((step, index) => {
-		console.log(index);
+		// creating a new element
 		let divContainer = document.createElement('div');
-		divContainer.classList.add('sidebar__steps__container');
 		let divStepNumber = document.createElement('div');
-		divStepNumber.classList.add('sidebar__step__number');
+		let divParagraphContainer = document.createElement('div');
 		let titleParagraph = document.createElement('p');
-		titleParagraph.classList.add('sidebar__step__title');
 		let stepParagraph = document.createElement('p');
+		// adding classes to new elements
+		divContainer.classList.add('sidebar__steps__container');
+		divStepNumber.classList.add('sidebar__step__number');
+		index === 0 && divStepNumber.classList.add('active');
+		divParagraphContainer.classList.add('sidebar__paragraph__container');
+		titleParagraph.classList.add('sidebar__step__title');
 		stepParagraph.classList.add('sidebar__step__paragraph__number');
+		// appending elements
 		sidebar.appendChild(divContainer);
 		divContainer.appendChild(divStepNumber);
-		divContainer.appendChild(stepParagraph);
-		divContainer.appendChild(titleParagraph);
+		divContainer.appendChild(divParagraphContainer);
+		divParagraphContainer.appendChild(stepParagraph);
+		divParagraphContainer.appendChild(titleParagraph);
+		// textContent
 		divStepNumber.textContent = index + 1;
 		stepParagraph.textContent = `step ${index + 1}`;
 		titleParagraph.textContent = step.stepTitle;
 	});
 };
+let activeStep = 0;
+
+const sidebarStepsActiveStateHandler = event => {
+	// console.log(event);
+	event.target.innerText === 'Next Step'
+		? activeStep < 3
+			? activeStep++
+			: activeStep
+		: activeStep > 0
+		? activeStep--
+		: activeStep;
+	if (event.target.innerText === 'Next Step') {
+		sidebarStepNumberIcons[activeStep].classList.add('active');
+		sidebarStepNumberIcons[activeStep - 1].classList.remove('active');
+	} else {
+		sidebarStepNumberIcons[activeStep].classList.add('active');
+		sidebarStepNumberIcons[activeStep + 1].classList.remove('active');
+	}
+};
+
+nextStepButton.addEventListener('click', sidebarStepsActiveStateHandler);
